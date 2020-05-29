502994fdca14fd739f24bf69126cf166aced01aa
pujol99 <alexpujolcomet@gmail.com>
2020-05-29 10:59:47 +0200

    wall

---
+++ Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.back * Time.deltaTime * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("person"))
+        {
+            speed = 0.0f;
+        }
+    }
+}
